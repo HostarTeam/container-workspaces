9c96b04971f0127dc30d0c181437afd17bdfc559
Uri Bar Ilan <ubarilan@gmail.com>
2022-08-31 02:01:43 +0300

    [Bugfix] Fixed bug (vuln) where you could respond to a task which is not yours (#41)
    Fixed bug (vuln) where you could respond to a task which is not yours

--- daemon/src/lib/ws/routing/wsCommand.ts
+++ daemon/src/lib/ws/routing/wsCommand.ts
@@ -22,5 +22,15 @@ export async function wsCommand(
     const handle: MessageRoutingHandler | undefined =
         MessageRouting[messageData.action];
 
+    const task = await this.getTask(messageData.taskid);
+    if (!task) return;
+
+    const reqIp = req.socket.remoteAddress;
+    const specifiedContainerIp = await this.proxmoxClient.getContainerIP(
+        task.containerID
+    );
+
+    if (reqIp !== specifiedContainerIp) return;
+
     if (handle) handle(this, req, messageData, socket);
 }
