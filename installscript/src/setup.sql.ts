5700de78c1407a22eb427c9e17ce0f29f04c7e7e
Idan <53697782+idandev@users.noreply.github.com>
2022-09-03 17:00:18 +0300

    Update setup.sql.ts

--- installscript/src/setup.sql.ts
+++ installscript/src/setup.sql.ts
@@ -8,60 +8,68 @@ export default `
 
 
 -- Dumping database structure for cw
-CREATE DATABASE IF NOT EXISTS \`cw\` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE \`cw\`;
+CREATE DATABASE IF NOT EXISTS `cw` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cw`;
+
+-- Dumping structure for table cw.clients
+CREATE TABLE IF NOT EXISTS `clients` (
+  `client_id` text NOT NULL,
+  `client_secret` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
 
 -- Dumping structure for table cw.config
-CREATE TABLE IF NOT EXISTS \`config\` (
-  \`config\` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
+CREATE TABLE IF NOT EXISTS `config` (
+  `config` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
 -- Dumping structure for table cw.cts
-CREATE TABLE IF NOT EXISTS \`cts\` (
-  \`id\` int NOT NULL,
-  \`ipv4\` varchar(16) NOT NULL,
-  \`ready\` tinyint NOT NULL DEFAULT '0',
-  PRIMARY KEY (\`id\`)
+CREATE TABLE IF NOT EXISTS `cts` (
+  `id` int NOT NULL,
+  `ipv4` varchar(16) NOT NULL,
+  `ready` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
 -- Dumping structure for table cw.ips
-CREATE TABLE IF NOT EXISTS \`ips\` (
-  \`id\` int NOT NULL AUTO_INCREMENT,
-  \`ipv4\` varchar(16) NOT NULL,
-  \`gateway\` varchar(16) NOT NULL,
-  \`netmask\` varchar(16) NOT NULL,
-  \`used\` tinyint NOT NULL DEFAULT '0',
-  PRIMARY KEY (\`id\`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `ips` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ipv4` varchar(16) NOT NULL,
+  `gateway` varchar(16) NOT NULL,
+  `netmask` varchar(16) NOT NULL,
+  `used` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
 -- Dumping structure for table cw.nodes
-CREATE TABLE IF NOT EXISTS \`nodes\` (
-  \`id\` int NOT NULL AUTO_INCREMENT,
-  \`nodename\` varchar(255) NOT NULL,
-  \`is_main\` tinyint NOT NULL DEFAULT '0',
-  \`ip\` varchar(16) NOT NULL,
-  \`location\` varchar(255) NOT NULL,
-  PRIMARY KEY (\`id\`)
+CREATE TABLE IF NOT EXISTS `nodes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nodename` varchar(255) NOT NULL,
+  `is_main` tinyint NOT NULL DEFAULT '0',
+  `ip` varchar(16) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
 
 -- Dumping structure for table cw.tasks
-CREATE TABLE IF NOT EXISTS \`tasks\` (
-  \`id\` varchar(24) NOT NULL,
-  \`containerID\` varchar(16) NOT NULL,
-  \`start_time\` bigint NOT NULL,
-  \`end_time\` bigint DEFAULT NULL,
-  \`data\` text,
-  \`status\` varchar(255) DEFAULT NULL,
-  \`error\` text,
-  PRIMARY KEY (\`id\`)
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `id` varchar(24) NOT NULL,
+  `containerID` varchar(16) NOT NULL,
+  `start_time` bigint NOT NULL,
+  `end_time` bigint DEFAULT NULL,
+  `data` text,
+  `status` varchar(255) DEFAULT NULL,
+  `error` text,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Data exporting was unselected.
